7031cbfa6fb62f5b64eca7ee75750c9bc2cc1cdf
vinayakat-p <vinayaka.t-p@capgemini.com>
2025-04-10 11:22:53 +0530

    Testing my second commit

---
+++ src/test/java/day1/AreaCalculator.java
@@ -0,0 +1,19 @@
+package day1;
+
+public class AreaCalculator {
+
+	// Method to calculate the area of a circle
+    double calculateArea(double radius) {
+        return Math.PI * radius * radius;
+    }
+
+    // Method to calculate the area of a rectangle
+    double calculateArea(double length, double width) {
+        return length * width;
+    }
+
+    // Method to calculate the area of a triangle
+    double calculateArea(double base, float height) {
+        return 0.5 * base * height;
+    }
+}
---
+++ src/test/java/day1/Main.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class Main {
+	public static void main(String[] args) {
+        AreaCalculator calculator = new AreaCalculator();
+
+        System.out.println("Area of Circle: " + calculator.calculateArea(5));
+        System.out.println("Area of Rectangle: " + calculator.calculateArea(4, 6));
+        System.out.println("Area of Triangle: " + calculator.calculateArea(3, 4));
+    }
+}
