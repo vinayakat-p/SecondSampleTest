2ca22c6285ffc025a8503c495f94e34a508e190b
vinayakat-p <vinayaka.t-p@capgemini.com>
2025-04-10 11:26:15 +0530

    Update Main.java
    Added some steps in main method

--- src/test/java/day1/Main.java
+++ src/test/java/day1/Main.java
@@ -7,5 +7,9 @@ public class Main {
         System.out.println("Area of Circle: " + calculator.calculateArea(5));
         System.out.println("Area of Rectangle: " + calculator.calculateArea(4, 6));
         System.out.println("Area of Triangle: " + calculator.calculateArea(3, 4));
+
+	System.out.println("Area of Circle: " + calculator.calculateArea(8));
+        System.out.println("Area of Rectangle: " + calculator.calculateArea(10, 60));
+        System.out.println("Area of Triangle: " + calculator.calculateArea(3, 594));
     }
 }
